85353785f2e85b0d80741deda094bd16c293c382
Philip Bove <pgbson@gmail.com>
2019-01-22 16:21:54 -0500

    added vlan tests

--- manager/database/Database.py
+++ manager/database/Database.py
@@ -40,23 +40,31 @@ class Database:
         return self.run_sql_stmt("DELETE FROM views WHERE view_name=%s", (view_name,), False)
 
     def create_vlan(self, vlan: Vlan, view_name: str):
-        return self.run_sql_stmt("INSERT into vlan VALUES (%s,%s,%s)",(vlan.name, vlan.number, view_name), False)
+        return self.run_sql_stmt("INSERT into vlan (name, number, view_name) VALUES (%s,%s,%s)",(vlan.name, vlan.number, view_name), False)
 
     def get_vlan(self, vlan: Vlan, view_name: str):
-        data = self.run_sql_stmt("SELECT * from vlan WHERE vlan_number=%s AND view_name=%s",(vlan.number, view_name), True)
-        return Vlan(name=data[0], number=data[1])
+        data = self.run_sql_stmt("SELECT name, number from vlan WHERE number=%s AND view_name=%s",(vlan.number, view_name), True)
+        if len(data) > 0:
+            logging.debug("get_vlan "+str(data))
+            return Vlan(name=data[0][0], number=data[0][1])
+        return None
 
     def delete_vlan(self, vlan: Vlan, view_name: str):
-        return self.run_sql_stmt("DELETE FROM vlan WHERE vlan_number=%s AND view_name=%s", (vlan.number, view_name), False)
+        return self.run_sql_stmt("DELETE FROM vlan WHERE number=%s AND view_name=%s", (vlan.number, view_name), False)
 
     def get_address(self, address: str, view_name: str):
         return self.run_sql_stmt("SELECT address from address WHERE address=%s and view_name=%s",
-                          (address, view_name), True)
+                          (address, view_name), True)[0][0]
 
-    def create_address(self, address: str, version: int, network: Network, view_name: str):
+    def get_addresses_in_use(self, network: str, view_name: str):
         return self.run_sql_stmt("""
-            INSERT into address (address, version, network_id ,view_name) VALUES (%s,%s,(SELECT network_id from network WHERE network=%s and view_name=%s),%s) 
-        """, (address, version, network.network, view_name, view_name), False)
+        SELECT COUNT(*) from address WHERE network_id=(SELECT network_id from network 
+        WHERE network=%s and view_name=%s) and view_name=%s and in_use=TRUE """,(network, view_name, view_name),True)[0][0]
+
+    def create_address(self, address: str, version: int, network: Network, in_use: bool,view_name: str):
+        return self.run_sql_stmt("""
+            INSERT into address (address, version, network_id ,view_name, in_use) VALUES (%s,%s,(SELECT network_id from network WHERE network=%s and view_name=%s),%s,%s) 
+        """, (address, version, network.network, view_name, view_name, in_use), False)
 
     def delete_address(self, address: str, view_name:str ):
         return self.run_sql_stmt("DELETE from address WHERE address=%s and view_name=%s",(address, view_name), False)
--- manager/tests/integration/test_database.py
+++ manager/tests/integration/test_database.py
@@ -1,6 +1,6 @@
 import pytest
-import logging
 import unittest.mock as mock
+from manager.ipam.objects.Vlan import Vlan
 from manager.database.Database import Database
 
 def test_create_view():
@@ -20,8 +20,22 @@ def test_addresses():
     db = Database('localhost', 'postgres', 'admin', 'manager', 5432, False)
     network = mock.Mock()
     network.network = '192.168.1.0/24'
-    assert(db.create_address('192.168.1.2', 4, network, 'test_view')) == True
-    assert(db.get_address('192.168.1.2', 'test_view'))[0] == ('192.168.1.2',)
+    assert(db.create_address('192.168.1.2', 4, network, True,'test_view')) == True
+    # create disabled address to verify that this only returns one
+    assert(db.create_address('192.168.1.3', 4, network, False, 'test_view')) == True
+    assert(db.get_address('192.168.1.2', 'test_view')) == '192.168.1.2'
+
+def test_get_address_in_use():
+    db = Database('localhost', 'postgres', 'admin', 'manager', 5432, False)
+    assert(db.get_addresses_in_use('192.168.1.0/24', 'test_view')) == 1
+
+def test_vlan():
+    db = Database('localhost', 'postgres', 'admin', 'manager', 5432, False)
+    vlan = Vlan('servers', 60)
+    assert(db.create_vlan(vlan, 'test_view')) == True
+    assert(db.get_vlan(vlan, 'test_view')) == vlan
+    assert(db.delete_vlan(vlan, 'test_view')) == True
+    assert(db.get_vlan(vlan, 'test_view')) is None
 
 def test_destroy_view():
      db = Database('localhost', 'postgres', 'admin', 'manager', 5432, False)
